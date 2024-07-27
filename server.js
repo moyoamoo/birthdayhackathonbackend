9b85a35b3d99fb953c17250a595a05bf12f55714
Moyo A-P <moyo.amoo@hotmail.com>
2024-07-27 10:02:13 +0100

    run server

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const helmet = require("helmet")
+const helmet = require("helmet");
+const exec = require("child_process").exec;
 
+app.use(cors());
+app.use(express.json());
+
+app.use(helmet());
+
+const PORT = process.env.PORT || 6001;
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
