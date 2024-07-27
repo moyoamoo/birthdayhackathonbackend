a58b82eae3a4f263873e61e9f243837cce460867
Sacha Uijlen <sacha-u@hotmail.com>
2024-07-27 16:02:52 +0100

    apiData

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ app.use(express.json());
 app.use(helmet());
 app.use("/products", require("./products"));
 
-app.use("/add_birthday", require("./post"));
-
 const PORT = process.env.PORT || 6001;
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
